4a422e55042a42a82f5881ffe50f323f839cc119
egdeliya <egdeliya@mail.ru>
2018-10-10 14:36:15 +0300

    collections/MergeSortImpl: mergeSort was done

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -7,6 +7,28 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  def mergeSort(data: Seq[Int]): Seq[Int] = data
+  private def merge(left: Seq[Int], right: Seq[Int]): Seq[Int] = {
+    def mergeAcc(curLeft: Int, curRight: Int, acc: Seq[Int]): Seq[Int] = {
+      if (curLeft >= left.size) acc ++ right.slice(curRight, right.size)
+      else if (curRight>= right.size) acc ++ left.slice(curLeft, left.size)
+      else if (left(curLeft) < right(curRight)) mergeAcc(curLeft + 1, curRight, acc :+ left(curLeft))
+      else mergeAcc(curLeft, curRight + 1, acc :+ right(curRight))
+    }
+    mergeAcc(0, 0, Seq.empty)
+  }
+
+  private def mergeSort(left: Int, right: Int, data: Seq[Int]): Seq[Int] = {
+    if (right - left < 2) data.slice(left, right)
+    else {
+      val mid = (left + right) / 2
+      val leftArr = mergeSort(left, mid, data)
+      val rightArr = mergeSort(mid, right, data)
+      merge(leftArr, rightArr)
+    }
+  }
+
+  def mergeSort(data: Seq[Int]): Seq[Int] = {
+    mergeSort(0, data.size, data)
+  }
 
 }

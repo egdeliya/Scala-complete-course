ee03912f275b7937ed1b4a755fcffd87ad081e25
Egdeliya Nurgalieva <egdeliya@mail.ru>
2018-10-11 19:23:37 +0300

    collections/MergeSortImpl: merge function was implemented with pattern matching

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -7,14 +7,12 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  private def merge(left: Seq[Int], right: Seq[Int]): Seq[Int] = {
-    def mergeAcc(curLeft: Int, curRight: Int, acc: Seq[Int]): Seq[Int] = {
-      if (curLeft >= left.size) acc ++ right.slice(curRight, right.size)
-      else if (curRight>= right.size) acc ++ left.slice(curLeft, left.size)
-      else if (left(curLeft) < right(curRight)) mergeAcc(curLeft + 1, curRight, acc :+ left(curLeft))
-      else mergeAcc(curLeft, curRight + 1, acc :+ right(curRight))
-    }
-    mergeAcc(0, 0, Seq.empty)
+  private def merge(left: Seq[Int], right: Seq[Int]): Seq[Int] = (left, right) match {
+      case (Nil, rightSeq) => rightSeq
+      case (leftSeq, Nil) => leftSeq
+      case (leftHead :: leftTail, rightHead :: rightTail) =>
+          if (leftHead < rightHead) Seq[Int](leftHead) ++ merge(leftTail, right)
+          else Seq[Int](rightHead) ++ merge(left, rightTail)
   }
 
   private def mergeSort(left: Int, right: Int, data: Seq[Int]): Seq[Int] = {
